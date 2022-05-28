59cbaa1e04916cb4e99acaf8959caf7614086c4f
Vangelis Spathonis <baggelisp.keph@gmail.com>
2022-05-28 17:26:08 +0300

    .

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 import { FavoritesService } from '../favorites/services/favorites.service';
 import { MovieDetailsService } from './services/movie-details.service';
 
@@ -11,6 +12,8 @@ import { MovieDetailsService } from './services/movie-details.service';
 })
 export class MovieDetailsPage implements OnInit {
   movieId: string;
+  isOnFavotites = false;
+  subscription: Subscription;
 
   constructor(private activatedRoute: ActivatedRoute, 
     private alertCtrl: AlertController,
@@ -20,15 +23,22 @@ export class MovieDetailsPage implements OnInit {
   ngOnInit() {
     this.movieId = this.activatedRoute.snapshot.paramMap.get('id');
     this.service.getMovie(parseInt(this.movieId));
+
+    this.subscription = this.service.movie$.subscribe( movie => {
+      if (!movie.id ) return;
+      this.isOnFavotites = this.favService.isMovieInFavorites(movie.id.toString())
+    })
   }
 
   async addToFavs(movieId: number, movieName: string){
     let title = 'Added!';
     let message = 'You added movie ' + movieName + ' to your favorites!';
+    this.isOnFavotites = true;
     if (this.favService.isMovieInFavorites(movieId.toString())) {
       this.favService.removeMovieToCollection(movieId.toString());
       message = 'You removed movie ' + movieName + ' to your favorites!';
       title = 'Removed!'
+      this.isOnFavotites = false;
     } else {
       this.favService.saveMovieToCollection(movieId.toString())
     }
@@ -40,5 +50,9 @@ export class MovieDetailsPage implements OnInit {
       buttons: ['OK']
     });
     await alert.present();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
